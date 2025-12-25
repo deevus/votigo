4d3c54e04ef60cf65d3e4a9abc09fa6aee035f5f
Simon Hartcher <simon@simonhartcher.com>
2025-12-25 15:17:20 +1100

    feat: implement category create command

--- cmd/category.go
+++ cmd/category.go
@@ -3,9 +3,12 @@ package cmd
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"os"
 	"text/tabwriter"
+
+	"github.com/palm-arcade/votigo/internal/db"
 )
 
 func (c *CategoryListCmd) Run(ctx *Context) error {
@@ -26,5 +29,26 @@ func (c *CategoryListCmd) Run(ctx *Context) error {
 	}
 	w.Flush()
 
+	return nil
+}
+
+func (c *CategoryCreateCmd) Run(ctx *Context) error {
+	var maxRank sql.NullInt64
+	if c.Type == "ranked" {
+		maxRank = sql.NullInt64{Int64: int64(c.MaxRank), Valid: true}
+	}
+
+	cat, err := ctx.Queries.CreateCategory(context.Background(), db.CreateCategoryParams{
+		Name:        c.Name,
+		VoteType:    c.Type,
+		Status:      "draft",
+		ShowResults: "after_close",
+		MaxRank:     maxRank,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Created category #%d: %s (%s)\n", cat.ID, cat.Name, cat.VoteType)
 	return nil
 }
